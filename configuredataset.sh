80e5f2aa727b395acd725fccf91fc2bd83a1b7e6
fifiramatou <fifiramatou@gmail.com>
2021-05-11 14:43:38 +0200

    Create configuredataset.sh

---
+++ configuredataset.sh
@@ -0,0 +1,9 @@
+
+path=$1
+echo $path
+noisy_train= $path + "/train/noisy"
+#if [[ ! -e $path ]]; then
+ #   mkdir -p $path
+#fi
+python3 -m denoiser.audio $noisy_train > egs/val/train/noisy.json 
+#python3 -m denoiser.audio dataset/debug/clean > $path/clean.json
