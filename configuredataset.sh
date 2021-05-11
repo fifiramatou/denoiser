131ea46f8773c8249ec8b66b3304a2b42ca2390e
fifiramatou <fifiramatou@gmail.com>
2021-05-11 17:10:06 +0200

    Update configuredataset.sh

--- configuredataset.sh
+++ configuredataset.sh
@@ -6,13 +6,19 @@ dataset_path=/content/$dataset_name
 echo "Répertoire du dataset:"$dataset_path
 #noisy_train_path=$1
 #egs_path=egs/val/train
-egs_path=egs/$dataset_name
-echo $egs_path
-if [[ ! -e $egs_path ]]; then
-   mkdir -p $egs_path
+egs_path_tr=egs/$dataset_name/tr
+echo $egs_path_tr
+egs_path_tt=egs/$dataset_name/tt
+echo $egs_path_tt
+if [[ ! -e $egs_path_tr ]]; then
+   mkdir -p $egs_path_tr
 fi
-python3 -m denoiser.audio $dataset_path/train/clean > $egs_path/tr/clean.json
-python3 -m denoiser.audio $dataset_path/train/noisy > $egs_path/tr/noisy.json 
+if [[ ! -e $egs_path_tt ]]; then
+   mkdir -p $egs_path_tt
+fi 
 
-python3 -m denoiser.audio $dataset_path/test/clean > $egs_path/tt/clean.json
-python3 -m denoiser.audio $dataset_path/test/noisy > $egs_path/tt/noisy.json 
+python3 -m denoiser.audio $dataset_path/train/clean > $egs_path_tr/clean.json
+python3 -m denoiser.audio $dataset_path/train/noisy > $egs_path_tr/noisy.json 
+
+python3 -m denoiser.audio $dataset_path/test/clean > $egs_path_tt/clean.json
+python3 -m denoiser.audio $dataset_path/test/noisy > $egs_path_tt/noisy.json 
