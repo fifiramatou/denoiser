149dfedc6d473fbf9a0172b529c19dbb23ff95ab
fifiramatou <fifiramatou@gmail.com>
2021-05-11 16:29:59 +0200

    Update configuredataset.sh

--- configuredataset.sh
+++ configuredataset.sh
@@ -1,10 +1,17 @@
-noisy_train_path=$1
-egs_path=egs/val/train
-echo $noisy_train_path 
-#chemin du dataset
-
+#nom du dataset
+dataset_name=$1
+echo "Nom du dataset:"$dataset_name 
+dataset_path=/kaggle/input/$dataset_name
+echo "Répertoire du dataset:"$dataset_path
+#noisy_train_path=$1
+#egs_path=egs/val/train
+egs_path="egs/{$dataset_name}"
+echo $egs_path
 if [[ ! -e $egs_path ]]; then
    mkdir -p $egs_path
 fi
-python3 -m denoiser.audio $noisy_train_path > $egs_path/noisy.json 
-#python3 -m denoiser.audio dataset/debug/clean > $path/clean.json
+python3 -m denoiser.audio $dataset_path/train/clean > $egs_path/tr/clean.json
+python3 -m denoiser.audio $dataset_path/train/noisy > $egs_path/tr/noisy.json 
+
+python3 -m denoiser.audio $dataset_path/test/clean > $egs_path/tt/clean.json
+python3 -m denoiser.audio $dataset_path/test/noisy > $egs_path/tt/noisy.json 
