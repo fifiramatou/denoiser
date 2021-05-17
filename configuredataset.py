410f3d73134c10ef1bccb22e3c6c09666d7efc2e
fifiramatou <fifiramatou@gmail.com>
2021-05-17 16:38:03 +0200

    Update configuredataset.py

--- configuredataset.py
+++ configuredataset.py
@@ -7,7 +7,7 @@ from yaml import SafeDumper
 
 
 def gen_conf_yaml(dataset):
-    config = [{'dset': [{'train': 'egs/'+dataset+"/tr"}, {'valid': None}, {'test': 'egs/'+dataset+'/tt'}, {'noisy_json': 'egs/'+dataset+'/tr/noisy.json'}, {'noisy_dir': No>                        {'matching': 'sort'}]}, {'eval_every': 2}]
+    config = [{'dset': [{'train': 'egs/'+dataset+"/tr"}, {'valid': None}, {'test': 'egs/'+dataset+'/tt'}, {'noisy_json': 'egs/'+dataset+'/tr/noisy.json'}, {'noisy_dir': None}, {'matching': 'sort'}]}, {'eval_every': 2}]
     SafeDumper.add_representer(type(None), lambda dumper,
                                value: dumper.represent_scalar(u'tag:yaml.org,2002:null', ''))
     with open("conf/dset/"+dataset+'.yaml', 'w') as f:
